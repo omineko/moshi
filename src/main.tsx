0415194d1900e1949a01f190778c762f3d6998e1
omineko <>
2023-04-01 21:41:23 +0800

    updated import order

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import firebaseConfig from './firebaseConfig';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import App from './App';
+import './index.css';
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
